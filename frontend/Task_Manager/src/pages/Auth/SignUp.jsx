4b27af3ed283e5c511f0268048607bd7fb2f979b
Adi224-os <adiveer224@gmail.com>
2026-02-16 20:45:34 +0530

    SignUp form UI created

--- frontend/Task_Manager/src/pages/Auth/SignUp.jsx
+++ frontend/Task_Manager/src/pages/Auth/SignUp.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
+import { validateEmail } from '../../utils/helper';
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
+import Input from '../../components/Inputs/Input';
+import { Link } from 'react-router-dom';
 
 function SignUp() {
   const [profilePic, setProfilePic] = useState(null);
@@ -10,17 +14,80 @@ function SignUp() {
 
   const [error, setError] = useState(null);
 
+  //handle SignUp form submit
+    const handleSignUp = async(e) => {
+      e.preventDefault();
+  
+      if(!fullName) {
+        setError("Please enter full name");
+        return;
+      }
+
+      if(!validateEmail(email)) {
+        setError("Please enter a valid email address.");
+        return;
+      }
+  
+      if(!password) {
+        setError("Please enter the password");
+        return;
+      }
+  
+      setError("");
+  
+      //SignUp  API call
+    };
+
   return (
     <AuthLayout>
-      <div className='lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center'>
+      <div className='lg:w-full h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center'>
         <h3 className='text-xl font-semibold text-black'>Create an Account</h3>
-        <p className='text-xs text-slate-700 mt-[5px] mb-6'>Enter your details below</p>
+        <p className='text-xs text-slate-700 mt-1.25 mb-6'>Enter your details below</p>
         <form onSubmit={handleSignUp}>
           <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
 
           <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+            <Input 
+              value={fullName}
+              onChange={({target}) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="John"
+              type="text"
+            />
 
+            <Input 
+            value={email}
+            onChange={({target}) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+            type="text"
+          />
+
+          <Input 
+            value={password}
+            onChange={({target}) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 characters"
+            type="password"
+          />
+
+          <Input 
+            value={adminInviteToken}
+            onChange={({target}) => setAdminInviteToken(target.value)}
+            label="Admin Invite Token"
+            placeholder="6 digits code"
+            type="text"
+          />
           </div>
+          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+          <button type='submit' className='btn-primary'>SIGN UP</button>
+
+          <p className='text-[13px] text-slate-800 mt-3'>
+            Already have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/login">Login</Link>
+          </p>
+          
         </form>
       </div>
     </AuthLayout>
