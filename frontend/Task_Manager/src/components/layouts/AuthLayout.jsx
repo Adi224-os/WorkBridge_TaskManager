73c5d52bd8be8a8ed6f10b8912ea29a34af40d12
Adi224-os <adiveer224@gmail.com>
2026-06-20 18:17:07 +0530

    SignUp API integration update

--- frontend/Task_Manager/src/components/layouts/AuthLayout.jsx
+++ frontend/Task_Manager/src/components/layouts/AuthLayout.jsx
@@ -5,7 +5,7 @@ import UI_IMG from "../../assets/images/auth-img.png";
 const AuthLayout = ({children}) => {
   return <div className='flex'>
         <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
-            <h2 className='text-lg font-medium text-black'>WorkBridge Task Manager</h2>
+            <h2 className='text-6xl font-bold text-blue-600'>WorkBridge Task Manager</h2>
             {children}
         </div>
 
